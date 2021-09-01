58b03396ff1f8b0b50313e70a3d4888b0099f5c0
Caio Schmidt <caiofonseca6@gmail.com>
2021-09-01 20:15:45 -0300

    Adicionado Fogao, criado Canvas

--- CafeGame/Assets/Scripts/MostraComida.cs
+++ CafeGame/Assets/Scripts/MostraComida.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class MostraComida : MonoBehaviour
 {
+    [SerializeField]
+    private Transform comidaOffset;
+
     public Comida comida;
+
+    public GameObject comidaAMostra;
 
     private void Start()
     {
-        Instantiate(comida.modelo, transform.position, Quaternion.identity);
+        //Instantiate(comida.modelo == null ? comidaAMostra : comida.modelo, comidaOffset.transform.position, comidaOffset.transform.rotation);
     }
 
     private void Update()
     {
         
+    }
+
+    public void EscolherComida(Comida com)
+    {
+        comida = com;
     }
 }
